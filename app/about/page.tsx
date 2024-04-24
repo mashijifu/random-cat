205a95acd8f67b26be56ce402695634461cca683
mashijifu <38903818+mashijifu@users.noreply.github.com>
2024-04-24 03:02:40 +0000

    Initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+import { NextPage } from "next";
+
+// export default function Home() {
+//   return (
+    
+//   );
+// }
+
+const AboutPage: NextPage = () => {
+  return <div>犬画像予定地</div>;
+};
+export default AboutPage;
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+import { NextPage } from "next";
+
+// export default function Home() {
+//   return (
+    
+//   );
+// }
+
+const IndexPage: NextPage = () => {
+  return <div>猫画像予定地</div>;
+};
+export default IndexPage;
